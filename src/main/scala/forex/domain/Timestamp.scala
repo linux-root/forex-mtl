d0c3036634e6610a1902847b62337299380735e2
Watson Dinh <superusr.root@gmail.com>
2023-12-06 15:42:53 +0700

    add Timestamp smart constructor

--- src/main/scala/forex/domain/Timestamp.scala
+++ src/main/scala/forex/domain/Timestamp.scala
@@ -1,10 +1,16 @@
 package forex.domain
 
-import java.time.OffsetDateTime
+import java.time.{ Instant, OffsetDateTime, ZoneId }
+import scala.util.Try
 
 case class Timestamp(value: OffsetDateTime) extends AnyVal
 
 object Timestamp {
   def now: Timestamp =
     Timestamp(OffsetDateTime.now)
+
+  def fromString(iso8601: String): Either[String, Timestamp] =
+    Try {
+      Right(Timestamp(Instant.parse(iso8601).atZone(ZoneId.systemDefault()).toOffsetDateTime))
+    }.getOrElse(Left("Cannot parse ISO 8601 datetime format"))
 }
