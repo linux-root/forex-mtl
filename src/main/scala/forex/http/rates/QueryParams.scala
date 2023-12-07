880134bace78e1ebb7474b9f6922a82bffa8c30c
Watson Dinh <superusr.root@gmail.com>
2023-12-07 11:42:51 +0700

    use safer QueryParam decoder

--- src/main/scala/forex/http/rates/QueryParams.scala
+++ src/main/scala/forex/http/rates/QueryParams.scala
@@ -1,13 +1,17 @@
 package forex.http.rates
 
 import forex.domain.Currency
-import org.http4s.QueryParamDecoder
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import org.http4s.{ParseFailure, QueryParamDecoder}
 
 object QueryParams {
 
   private[http] implicit val currencyQueryParam: QueryParamDecoder[Currency] =
-    QueryParamDecoder[String].map(Currency.unsafeFromString)
+    QueryParamDecoder[String].emap { s =>
+      Currency.fromString(s).left.map(failure =>
+        ParseFailure(s, failure.message)
+      )
+    }
 
   object FromQueryParam extends QueryParamDecoderMatcher[Currency]("from")
   object ToQueryParam extends QueryParamDecoderMatcher[Currency]("to")
